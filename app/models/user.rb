85c146c676c6362b4faaf56058b4e5b888e76a21
KenFukui <79432343+KenFukui@users.noreply.github.com>
2021-05-18 21:23:11 +0900

    rubocopの実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,18 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])\w{6,225}\z/
-         validates :password, format: { with: VALID_PASSWORD_REGEX, message: "is invalid. Include both letters and numbers" }
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)\w{6,225}\z/
+  validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers' }
 
-         validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters" }
-         validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters" }
-         validates :last_read, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters" }
-         validates :first_read, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters" }
+  validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'is invalid. Input full-width characters' }
+  validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'is invalid. Input full-width characters' }
+  validates :last_read, format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters' }
+  validates :first_read, format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters' }
 
-         validates :nickname, presence: true
-         validates :last_name, presence: true
-         validates :first_name, presence: true
-         validates :last_read, presence: true
-         validates :first_read, presence: true
-         validates :birthday, presence: true
+  validates :nickname, presence: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_read, presence: true
+  validates :first_read, presence: true
+  validates :birthday, presence: true
 end
