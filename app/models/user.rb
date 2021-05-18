8e87948adc196abfcca8d84bdcd8b672a566f918
KenFukui <79432343+KenFukui@users.noreply.github.com>
2021-05-18 17:15:40 +0900

    新規登録、ログイン、ログアウト機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
+         validates :password, format: { with: VALID_PASSWORD_REGEX }
+
+         validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters" }
+         validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters" }
+         validates :last_read, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters" }
+         validates :first_read, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters" }
+
+         validates :nickname, presence: true
+         validates :last_name, presence: true
+         validates :first_name, presence: true
+         validates :last_read, presence: true
+         validates :first_read, presence: true
+         validates :birthday, presence: true
 end
