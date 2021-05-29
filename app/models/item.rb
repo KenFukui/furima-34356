127d95596a14375bcda671ba34c89328a8415572
KenFukui <79432343+KenFukui@users.noreply.github.com>
2021-05-29 14:29:26 +0900

    修正３

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,8 @@
 class Item < ApplicationRecord
   has_one_attached :image
   belongs_to :user
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :fee
 
   with_options presence: true do
     validates :image
